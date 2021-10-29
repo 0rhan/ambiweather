487fd44689ecfe8a3e541b626ab2581905c65807
0rhan <gurbanov.inbox@gmail.com>
2021-10-29 19:14:06 +0300

    feat(UI): Components for icons

---
+++ src/UI/Components/DataDisplay/Icons/WeatherSkyIcons/index.tsx
@@ -0,0 +1,33 @@
+// THUNDERSTORM
+export {ReactComponent as SkyThunderstormWithRain} from "assets/icons/weather/animated/thunderstorms-rain.svg"
+export {ReactComponent as SkyThunderstorm} from "assets/icons/weather/animated/thunderstorms.svg"
+
+// DRIZZLE
+export {ReactComponent as SkyDrizzle} from "assets/icons/weather/animated/drizzle.svg"
+
+// RAIN
+export {ReactComponent as SkyRain} from "assets/icons/weather/animated/rain.svg"
+
+//SNOW
+export {ReactComponent as SkySnow} from "assets/icons/weather/animated/snow.svg"
+export {ReactComponent as SkySleet} from "assets/icons/weather/animated/sleet.svg"
+
+// ATMOSPHERE 
+export {ReactComponent as SkyMist} from "assets/icons/weather/animated/mist.svg"
+export {ReactComponent as SkySmoke} from "assets/icons/weather/animated/smoke.svg"
+export {ReactComponent as SkyHaze} from "assets/icons/weather/animated/haze.svg"
+export {ReactComponent as SkyFog} from "assets/icons/weather/animated/fog.svg"
+export {ReactComponent as SkyDustWind} from "assets/icons/weather/animated/dust-wind.svg"
+export {ReactComponent as SkyDust} from "assets/icons/weather/animated/dust.svg"
+
+// CLEAR SKY 
+export {ReactComponent as SkyClearDay} from "assets/icons/weather/animated/clear-day.svg"
+export {ReactComponent as SkyClearNight} from "assets/icons/weather/animated/clear-night.svg"
+
+// CLOUDS
+export {ReactComponent as SkyCloudy} from "assets/icons/weather/animated/cloudy.svg"
+export {ReactComponent as SkyOvercast} from "assets/icons/weather/animated/overcast.svg"
+
+
+// NOT AVAILABLE 
+export {ReactComponent as NotAvailable} from "assets/icons/weather/animated/not-available.svg"
