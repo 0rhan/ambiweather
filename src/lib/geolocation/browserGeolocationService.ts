3c4601275419c94d8aed06c02c4baf56cd206bdd
0rhan <gurbanov.inbox@gmail.com>
2021-11-03 22:13:16 +0300

    feat(lib): Browser geolocation service

--- src/lib/geolocation/browserGeolocationService.ts
+++ src/lib/geolocation/browserGeolocationService.ts
@@ -23,7 +23,7 @@ class BrowserGeoLocationService {
      false means "not allowed"
      true means "allowed" 
   */
-  geoLocationPermission: Boolean = false;
+  geoLocationPermission: boolean = false;
 
   geoCoords: GeoCoords | undefined = undefined;
 
@@ -33,7 +33,7 @@ class BrowserGeoLocationService {
   );
 
   private checkGeolocationAPIIAvailability(): void {
-    const geolocationAPINotAvailable:boolean = !this.geoLocationAPIIsAvailable;
+    const geolocationAPINotAvailable: boolean = !this.geoLocationAPIIsAvailable;
 
     /* Check if browser have Geolocation API
      Throw error when browser doesn't have Geolocation API */
@@ -52,20 +52,21 @@ class BrowserGeoLocationService {
     return false;
   }
 
-  public readGeoCoordsFromLocalStorage(): GeoCoords {
+  public readGeoCoordsFromLocalStorage(): GeoCoords | null {
     const geoLatString: geoDataFromLS = localStorage.getItem(
       LOCAL_STORAGE_GEO_LAT_KEY
     );
     const geoLonString: geoDataFromLS = localStorage.getItem(
       LOCAL_STORAGE_GEO_LON_KEY
     );
-
-    const geoLatNumber: number = Number(geoLatString);
-    const geoLonNumber: number = Number(geoLonString);
-
-    const geoCoords: GeoCoords = { lat: geoLatNumber, lon: geoLonNumber };
-
-    return geoCoords;
+    if (geoLatString !== null && geoLonString !== null) {
+      const geoLatNumber: number = Number(geoLatString);
+      const geoLonNumber: number = Number(geoLonString);
+      const geoCoords: GeoCoords = { lat: geoLatNumber, lon: geoLonNumber };
+      return geoCoords;
+    } else {
+      return null;
+    }
   }
 
   public writeGeoCoordsToLocalStorage(geoCoords: GeoCoords): Boolean {
@@ -106,26 +107,25 @@ class BrowserGeoLocationService {
       return geolocationPromiseResult;
     } catch (error) {
       // set permission status to false
-      if(error instanceof GeolocationPositionError) {
+      if (error instanceof GeolocationPositionError) {
         if (error.code === 1) {
           this.geoLocationPermission = false;
         }
-        console.log("error in GeoLocationService", error);
-        return error;
+        throw error;
       } else {
-          return new GeolocationPositionError()
+        return new GeolocationPositionError();
       }
-      }
+    }
   }
 
-  private async startLocationService(): Promise<GeoCoords | GeolocationPositionError | void> {
+  private async startLocationService(): Promise<
+    GeoCoords | GeolocationPositionError | void
+  > {
     try {
       const geoLocationPromiseResult: GeoLocationPromiseResult =
         await this.asyncGetCurrentPosition();
-      console.log("geolocationPromiseResult", geoLocationPromiseResult);
 
       if (geoLocationPromiseResult instanceof GeolocationPosition) {
-        console.log(geoLocationPromiseResult);
         const {
           coords: { latitude: lat, longitude: lon },
         } = geoLocationPromiseResult;
@@ -134,16 +134,19 @@ class BrowserGeoLocationService {
         this.geoCoords = geoCoords;
       }
     } catch (error) {
-      console.log("error in GeoLocationService", error);
-      if(error instanceof GeolocationPositionError) {
-        return error;
+      if (error instanceof GeolocationPositionError) {
+        throw error;
       }
     }
   }
 
   public async getGeoCoordsFromBrowserAPI(): Promise<GeoCoords | void> {
-    await this.startLocationService();
-    if (this.geoCoords !== undefined) return this.geoCoords;
+    try {
+      await this.startLocationService();
+      if (this.geoCoords !== undefined) return this.geoCoords;
+    } catch (error) {
+      if (error instanceof Error) throw Error;
+    }
   }
 }
 
